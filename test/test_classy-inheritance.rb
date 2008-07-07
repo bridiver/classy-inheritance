d8b0b78e397cc3d5ae91c3cc3e53ed754bc4db52
Geoff Garside <geoff@geoffgarside.co.uk>
2008-07-07 16:51:13 +0100

    Add tests for classy inheritance

--- test/test_classy-inheritance.rb
+++ test/test_classy-inheritance.rb
@@ -1,11 +1,57 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
+User.depends_on :profile, :attrs => [:first_name, :last_name, :email]
+
 class TestClassyInheritance < Test::Unit::TestCase
 
   def setup
+    @user = User.new
   end
   
-  def test_truth
-    assert true
+  def test_active_record_should_have_classy_inheritance_included
+    assert ActiveRecord::Base.included_modules.include?(Stonean::ClassyInheritance)
   end
+  
+  def test_active_record_should_respond_to_depends_on
+    assert ActiveRecord::Base.respond_to?(:depends_on)
+  end
+  
+  def test_user_should_respond_to_first_name
+    assert @user.respond_to?(:first_name)
+  end
+  
+  def test_user_should_respond_to_first_name=
+    assert @user.respond_to?(:first_name=)
+  end
+  
+  def test_user_should_respond_to_last_name
+    assert @user.respond_to?(:last_name)
+  end
+  
+  def test_user_should_respond_to_last_name=
+    assert @user.respond_to?(:last_name=)
+  end
+  
+  def test_user_should_respond_to_email
+    assert @user.respond_to?(:email)
+  end
+  
+  def test_user_should_respond_to_email=
+    assert @user.respond_to?(:email=)
+  end
+  
+  def test_user_should_be_invalid_without_profile_attributes
+    @user.login = 'joe'
+    assert !@user.valid?
+  end
+  
+  def test_user_should_valid_with_profile_attributes
+    @user.login = 'joe'
+    @user.first_name = 'joe'
+    @user.last_name = 'bloggs'
+    @user.email = 'joe@bloggs.co.uk'
+    
+    assert @user.valid?
+  end
+  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,45 @@
+require 'rubygems'
+require 'activerecord'
+
 require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/classy-inheritance'
+
+ActiveRecord::Base.establish_connection({
+      :adapter => "sqlite3",
+      :dbfile => "test/database.sqlite3"
+})
+
+class SetupTestTables < ActiveRecord::Migration
+  def self.up
+    create_table :profiles, :force => true do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+
+      t.timestamps
+    end
+    
+    create_table :users, :force => true do |t|
+      t.string :login
+      t.integer :profile_id
+
+      t.timestamps
+    end
+  end
+  
+  def self.down
+    drop_table :users
+    drop_table :profiles
+  end
+end
+
+SetupTestTables.migrate(:up)
+
+class Profile < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name, :email
+end
+
+class User < ActiveRecord::Base
+  validates_presence_of :login
+end
+
