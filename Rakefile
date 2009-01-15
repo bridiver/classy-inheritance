6b31e09e32f91b68195a704f21adf9764ffa5276
Andrew Stone <andy@stonean.com>
2009-01-14 21:21:42 -0500

    fix from johnsbrn for prefix and postfix validations

--- Rakefile
+++ Rakefile
@@ -1,4 +1,30 @@
-require 'config/requirements'
-require 'config/hoe' # setup Hoe + all gem configuration
+# Look in the tasks/setup.rb file for the various options that can be
+# configured in this Rakefile. The .rake files in the tasks directory
+# are where the options are used.
 
-Dir['tasks/**/*.rake'].each { |rake| load rake }
+begin
+  require 'bones'
+  Bones.setup
+rescue LoadError
+  begin
+    load 'tasks/setup.rb'
+  rescue LoadError
+    raise RuntimeError, '### please install the "bones" gem ###'
+  end
+end
+
+ensure_in_path 'lib'
+require 'classy-inheritance'
+
+task :default => 'spec:run'
+
+PROJ.name = 'classy-inheritance'
+PROJ.authors = 'Andrew Stone'
+PROJ.email = 'andy@stonean.com'
+PROJ.url = 'http://stonean.com/wiki/classy-inheritance'
+PROJ.version = Stonean::ClassyInheritance::VERSION
+PROJ.rubyforge.name = 'classyinherit'
+
+PROJ.spec.opts << '--color'
+
+# EOF
--- lib/classy-inheritance.rb
+++ lib/classy-inheritance.rb
@@ -1,28 +1,10 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-
-module ActiveRecord::Validations::ClassMethods
-  def validates_associated(*attr_names)
-    configuration = { :message => ActiveRecord::Errors.default_error_messages[:invalid], :on => :save }
-    configuration.update(attr_names.extract_options!)
-
-    validates_each(attr_names, configuration) do |record, attr_name, value|
-      associate = record.send(attr_name)
-      if associate && !associate.valid?
-        associate.errors.each do |key, value|
-          record.errors.add(key, value) unless record.errors[key]
-        end
-      end
-    end
-  end
-end
-                                  
 
 module Stonean
   module ClassyInheritance
-    def self.included(base)
-      base.extend Stonean::ClassyInheritance::ClassMethods
+    VERSION = '0.6.3'
+
+    def self.version
+      VERSION
     end
 
     module ClassMethods
@@ -40,10 +22,10 @@ module Stonean
 
         if options.has_key?(:validates_associated_if) && options[:validates_associated_if] != true
           if [Symbol, String, Proc].include?(options[:validates_associated_if].class)
-            validates_associated model_sym, :if => options[:validates_associated_if]
+            validates_associated_dependent model_sym, options, :if => options[:validates_associated_if]
           end
         else
-          validates_associated model_sym
+          validates_associated_dependent model_sym, options 
         end
 
         # Before save functionality to create/update the requisite object
@@ -179,7 +161,7 @@ module Stonean
         requisite_klass = eval(klass)
         unless requisite_klass.respond_to?(self.name.underscore.to_sym)
           requisite_klass.send :can_be, self.name.underscore, 
-                                      :as => polymorphic_name 
+            :as => polymorphic_name 
         end
       end
 
@@ -190,4 +172,32 @@ module Stonean
     end # ClassMethods
   end # ClassyInheritance module
 end # Stonean module
-ActiveRecord::Base.send :include, Stonean::ClassyInheritance
+
+if Object.const_defined?("ActiveRecord") && ActiveRecord.const_defined?("Base")
+  module ActiveRecord::Validations::ClassMethods
+
+    def validates_associated_dependent(model_sym, options, configuration = {})
+      configuration = { :message => ActiveRecord::Errors.default_error_messages[:invalid], :on => :save }.update(configuration)
+
+      validates_each(model_sym, configuration) do |record, attr_name, value|
+        associate = record.send(attr_name)
+        if associate && !associate.valid?
+          associate.errors.each do |key, value|
+            if options[:prefix]
+              key = (options[:prefix] == true) ? "#{model_sym}_#{key}" : "#{options[:prefix]}_#{key}"
+            end
+            if options[:postfix]
+              key = (options[:postfix] == true) ? "#{key}_#{model_sym}" : "#{key}_#{options[:postfix]}"
+            end
+            record.errors.add(key, value) unless record.errors[key]
+          end
+        end
+      end
+    end
+  end
+                                    
+
+  ActiveRecord::Base.class_eval do
+    extend Stonean::ClassyInheritance::ClassMethods
+  end
+end
